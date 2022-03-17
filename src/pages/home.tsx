8ba2f12525767122336754731784ab88203b06e5
ak <123@qq.com>
2022-03-17 17:05:55 +0800

    add header, make grids 添加空隙 完成页面背景和 div 边框 柱形图搞定 重构 Chart1

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import './home.scss';
+import headerBg from '../images/header.png';
+import {Chart1} from '../components/chart-1';
 
+const px = (n) => n / 2420 * (window as any).pageWidth;
 export const Home = () => {
-  return (
-    <div>
-<div className="x"></div>
-
-    </div>
-  );
+    return (
+        <div className="home">
+            <header style={{backgroundImage: `url(${headerBg})`}}/>
+            <main>
+                <section className="section1">
+                    <Chart1/>
+                </section>
+                <section className="section2"></section>
+                <section className="section3"></section>
+                <section className="section4"></section>
+                <section className="section5"></section>
+            </main>
+        </div>
+    );
 };
