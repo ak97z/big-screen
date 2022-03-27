455aa086faff0e3c92165758caef7679e50d4e49
ak <123@qq.com>
2022-03-27 15:33:19 +0800

    chart-8.tsx数据动态化

--- src/components/chart-8.tsx
+++ src/components/chart-8.tsx
@@ -5,8 +5,37 @@ import {createEchartsOptions} from '../shared/create-echarts-options';
 export const Chart8 = () => {
     const divRef = useRef(null);
     const colors = ['#856BED', '#F46064', '#F38E1C', '#1CDB7C', '#33A4FA'];
+    const data = [
+        {value: 0.07, name: '10-20'},
+        {value: 0.10, name: '20-30'},
+        {value: 0.23, name: '30-40'},
+        {value: 0.28, name: '40-50'},
+        {value: 0.32, name: '50-60'},
+    ];
     useEffect(() => {
-        var myChart = echarts.init(divRef.current);
+        setInterval(() => {
+            function getRandomInt(max) {
+                return Math.floor(Math.random() * max);
+            }
+            let A=getRandomInt(40);
+            let B = getRandomInt(100-A);
+            let C = getRandomInt(100-A-B);
+            let D = getRandomInt(100-A-B-C);
+            let E = 100-A-B-C-D;
+            console.log(A + B + C + D + E);
+
+            const newData = [
+                {value: A, name: '10-20'},
+                {value: B, name: '20-30'},
+                {value: C, name: '30-40'},
+                {value:D, name: '40-50'},
+                {value:E, name: '50-60'},
+            ];
+            x(newData);
+        }, 1000);
+        const x = (data) => {
+
+            var myChart = echarts.init(divRef.current);
         myChart.setOption(createEchartsOptions({
             color: colors,
             xAxis: {show: false},
@@ -19,7 +48,7 @@ export const Chart8 = () => {
                     label: {
                         show: true, position: 'inside', textStyle: {color: 'white', fontSize: px(20)},
                         formatter(options) {
-                            return (options.value * 100).toFixed(0) + '%';
+                            return (options.value).toFixed(0) + '%';
                         }
                     },
                     labelLine: {show: false},
@@ -27,16 +56,12 @@ export const Chart8 = () => {
                         borderColor: '#0F113A',
                         borderWidth: px(4)
                     },
-                    data: [
-                        {value: 0.07, name: '10-20'},
-                        {value: 0.10, name: '20-30'},
-                        {value: 0.23, name: '30-40'},
-                        {value: 0.28, name: '40-50'},
-                        {value: 0.32, name: '50-60'},
-                    ]
+                    data: data.map(i => i),
                 }
             ]
         }))
+
+        }
 
     }, []);
 
